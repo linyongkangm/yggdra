900dcaabbec973d15e6e6c49c850f83f0babaf6d
linyongkangm <linyongkangm@foxmail.com>
2023-03-29 00:26:47 +0800

    feat: 生成中间人证书

--- yggdra-bin/ca/ca.go
+++ yggdra-bin/ca/ca.go
@@ -3,13 +3,11 @@ package ca
 import (
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"io/ioutil"
 	"math/big"
-	"net"
 	"os"
 	"time"
 	"yggdra/config"
@@ -45,6 +43,7 @@ func GenPrivateKey() (key *rsa.PrivateKey) {
 	return
 }
 
+// 生成根证书
 func GenRootCertificate() {
 	rootCsr := GenRootCertificateSign()
 	rootKey := GenPrivateKey()
@@ -70,41 +69,54 @@ func GenRootCertificate() {
 	pem.Encode(keyFile, &block)
 }
 
-func GenCertificate() {
-	max := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, _ := rand.Int(rand.Reader, max)
-	subject := pkix.Name{
-		Country:            []string{"CN"},
-		Province:           []string{"BeiJing"},
-		Organization:       []string{"yggdra"},
-		OrganizationalUnit: []string{"certYggdra"},
-		CommonName:         "yggdra",
+func GenmMitMCertificateSign(commonName string) (csr *x509.Certificate) {
+	csr = &x509.Certificate{
+		Version:      3,
+		SerialNumber: big.NewInt(time.Now().Unix()),
+		Subject: pkix.Name{
+			Country:            []string{"CN"},
+			Province:           []string{"GuangZhou"},
+			Locality:           []string{"GuangZhou"},
+			Organization:       []string{"Yggdra"},
+			OrganizationalUnit: []string{"YggdraProxy"},
+			CommonName:         commonName,
+		},
+		DNSNames:              []string{commonName},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0),
+		BasicConstraintsValid: true,
+		IsCA:                  false,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 	}
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject:      subject,
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
-		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
-	}
-
-	pk, _ := rsa.GenerateKey(rand.Reader, 2048)
-
-	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &pk.PublicKey, pk)
-	certOut, _ := os.Create(config.ROOT_CA_CRT)
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	certOut.Close()
-
-	keyOut, _ := os.Create(config.ROOT_CA_KEY)
-	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
-	keyOut.Close()
+	return
 }
 
-func GetTLSConfig() (cert tls.Certificate, err error) {
-	rawCert, _ := ioutil.ReadFile(config.ROOT_CA_CRT)
-	rawKey, _ := ioutil.ReadFile(config.ROOT_CA_KEY)
-	return tls.X509KeyPair(rawCert, rawKey)
+func GenMitMCertificate(commonName string) {
+	rootCsrBytes, _ := ioutil.ReadFile(config.ROOT_CA_CRT)
+	rootCsr, _ := x509.ParseCertificate(rootCsrBytes)
+	rootKeyBytes, _ := ioutil.ReadFile(config.ROOT_CA_KEY)
+	rootKey, _ := x509.ParsePKCS1PrivateKey(rootKeyBytes)
+	csr := GenmMitMCertificateSign(commonName)
+	key := GenPrivateKey()
+	der, _ := x509.CreateCertificate(rand.Reader, csr, rootCsr, key.Public(), rootKey)
+	//4.将得到的证书放入pem.Block结构体中
+	block := pem.Block{
+		Type:    "CERTIFICATE",
+		Headers: nil,
+		Bytes:   der,
+	}
+	crtFile, _ := os.Create(config.ROOT_CA_CRT)
+	defer crtFile.Close()
+	pem.Encode(crtFile, &block)
+
+	//6.将私钥中的密钥对放入pem.Block结构体中
+	block = pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   x509.MarshalPKCS1PrivateKey(key),
+	}
+	keyFile, _ := os.Create(config.ROOT_CA_KEY)
+	defer keyFile.Close()
+	pem.Encode(keyFile, &block)
 }
